eef662bad3575dcd3a77ed36262c98cb8cbcbfd1
hrsas <rharshvardhan96@gmail.com>
2025-10-01 11:00:31 +0530

    Added Problem 1: LED on Port J Bit 0

--- ETALVIS_LED_Problem_01_01/src/sketch.ino
+++ ETALVIS_LED_Problem_01_01/src/sketch.ino
@@ -8,10 +8,6 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   volatile char *x;
-  volatile long i;
   x = 0x105;
   *x = 1;
-  for(i = 0; i<100000; i++);
-  *x = 0;
-  for(i = 0; i<100000; i++);
 }
